7c62644e4d9d491db46873dee058f83c1986e317
chan-w <40780153+chan-w@users.noreply.github.com>
2023-10-19 22:27:08 -0500

    Add example for large file and modify binary search examples

--- src/binary-search-examples.ts
+++ src/binary-search-examples.ts
@@ -4,7 +4,8 @@ import { python } from '@codemirror/lang-python';
 import clozePlugin  from './cloze-editor';
 
 new EditorView({
-    doc: `def search_last(arr, target):
+    doc: `def search_any(arr, target):
+    """search_any([1, 2, 2, 2, 3], 2) => 2"""
     left, right = 0, len(arr) - 1
     result = -1
     while left <= right:
@@ -24,6 +25,7 @@ new EditorView({
 
   new EditorView({
     doc:  `def search_first(arr, target):
+    """search_any([1, 2, 2, 2, 3], 2) => 1"""
     left, right = 0, len(arr) - 1
     result = -1
     while left <= right:
@@ -43,6 +45,7 @@ new EditorView({
 
 new EditorView({
     doc:  `def search_last(arr, target):
+    """search_any([1, 2, 2, 2, 3], 2) => 3"""
     left, right = 0, len(arr) - 1
     result = -1
     while left <= right:
