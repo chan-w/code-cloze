5c904bd0531681f2ede732cd10f24100424bf2a7
chan-w <40780153+chan-w@users.noreply.github.com>
2023-10-19 22:27:08 -0500

    Update cloze toggle

--- src/cloze-editor.ts
+++ src/cloze-editor.ts
@@ -1,19 +1,43 @@
 import { EditorState } from '@codemirror/state';
-import { basicSetup } from "codemirror";
+import { minimalSetup, basicSetup } from "codemirror";
 import { Decoration, EditorView, ViewUpdate, ViewPlugin, DecorationSet, WidgetType } from "@codemirror/view"
+import { javascript } from '@codemirror/lang-javascript';
 
 class ClozeWidget extends WidgetType {
-  constructor(readonly start: number, readonly end: number) { super() }
+  private revealed: boolean;
+  private clozeContent: string;
+  
+  constructor(readonly start: number, readonly end: number, content: string) {
+    super(); 
+    this.revealed = false;
+    this.clozeContent = content
+  }
 
   eq(other: ClozeWidget) { return other.start === this.start && other.end === this.end }
 
   toDOM() {
-      let wrap = document.createElement("span")
-      wrap.setAttribute("aria-hidden", "true")
-      wrap.className = "cm-boolean-toggle"
-      let cloze = wrap.appendChild(document.createElement("p"))
-      cloze.innerText = `[${this.start} ... ${this.end}]`
-      return wrap
+    let wrap = document.createElement("span");
+    wrap.setAttribute("aria-hidden", "true");
+    wrap.className = "cb";
+    wrap.style.display = "inline-flex";
+    wrap.style.alignItems = "center";
+  
+    const clozeP = `[...]`;
+    let cloze = wrap.appendChild(document.createElement("p"));
+    cloze.innerText = this.revealed ? this.clozeContent : clozeP;
+    cloze.style.margin = "0";
+    cloze.style.display = "inline";
+  
+    let toggleButton = wrap.appendChild(document.createElement("button"));
+    toggleButton.innerText = this.revealed ? "Hide Answer" : "Show Answer";
+    toggleButton.style.marginLeft = "5px";
+    toggleButton.addEventListener("click", () => {
+      this.revealed = !this.revealed;
+      cloze.innerText = this.revealed ? this.clozeContent : clozeP;
+      toggleButton.innerText = this.revealed ? "Hide Answer" : "Show Answer";
+    });
+  
+    return wrap;
   }
 
   ignoreEvent() { return false }
@@ -34,17 +58,19 @@ const getClozePositions = (content: string): Array<{ from: number, to: number }>
 const getClozeDecorations = (state: EditorState): any => {
   const widgets: any[] = [];
   const clozePositions = getClozePositions(state.doc.toString());
+  const docContent = state.doc.toString();
 
   for (const { from, to } of clozePositions) {
     let deco = Decoration.replace({
-      widget: new ClozeWidget(from, to),
-      side: 1
+        widget: new ClozeWidget(from, to, docContent.slice(from, to)),
+        side: 1
     })
     widgets.push(deco.range(from, to))
   }
 
   return Decoration.set(widgets);
 };
+
 
 const clozePlugin = ViewPlugin.fromClass(class {
   decorations: DecorationSet
@@ -54,15 +80,17 @@ const clozePlugin = ViewPlugin.fromClass(class {
   }
 
   update(update: ViewUpdate) {
-    if (update.docChanged || update.viewportChanged)
+    if (update.docChanged || update.viewportChanged || update.selectionSet)
       this.decorations = getClozeDecorations(update.view.state)
   }
 }, {
   decorations: v => v.decorations
 })
 
-const view = new EditorView({
-    doc: `Sample text with a {{cloze deletion}}.\nAnother {{cloze}} deletion`,
-    extensions: [basicSetup, clozePlugin],
-    parent: document.body
-  });
+const noEdit = EditorView.editable.of(false)
+
+const view_1 = new EditorView({
+  doc: `const y = {{true}} // comment \nconst x = {{true}}\nconst z = x && y // z true`,
+  extensions: [basicSetup, clozePlugin, javascript(), noEdit, EditorView.lineWrapping],
+  parent: document.getElementById("js-demo-1")
+});
